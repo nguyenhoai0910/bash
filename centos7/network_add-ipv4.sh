8ad6ff68bcbf768cf54941c4242bd1a519d3cd2b
Nguyen Hoai <50800569+nguyenhoai0910@users.noreply.github.com>
2022-08-01 14:18:19 +0700

    Update network_add-ipv4.sh

--- centos7/network_add-ipv4.sh
+++ centos7/network_add-ipv4.sh
@@ -9,7 +9,12 @@ cat /etc/sysconfig/network-scripts/ifcfg-ens34|grep BOOTPROTO
 sed -i s/BOOTPROTO=dhcp/BOOTPROTO=static/g /etc/sysconfig/network-scripts/ifcfg-ens34 &> /dev/null
 sed -i s/ONBOOT=no/ONBOOT=yes/g /etc/sysconfig/network-scripts/ifcfg-ens34 &> /dev/null
 cat /etc/sysconfig/network-scripts/ifcfg-ens34|grep BOOTPROTO
-sleep 5
+sleep 2
+sed -i '/IPADDR/d' /etc/sysconfig/network-scripts/ifcfg-ens34
+sed -i '/NETMASK/d' /etc/sysconfig/network-scripts/ifcfg-ens34
+sed -i '/GATEWAY/d' /etc/sysconfig/network-scripts/ifcfg-ens34
+sed -i '/DNS1/d' /etc/sysconfig/network-scripts/ifcfg-ens34
+sleep 3
 /bin/cat<<EOM >> /etc/sysconfig/network-scripts/ifcfg-ens34
 IPADDR=192.168.56.10
 NETMASK=255.255.255.0
