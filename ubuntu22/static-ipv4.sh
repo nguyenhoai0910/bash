6084b515da367c394793be5082d9b69846b0098d
Nguyen Hoai <50800569+nguyenhoai0910@users.noreply.github.com>
2023-09-03 09:58:33 +0700

    Update static-ipv4.sh

--- ubuntu22/static-ipv4.sh
+++ ubuntu22/static-ipv4.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-cat /etc/netplan/*-installer-config.yaml
+ll /etc/netplan/*-installer-config.yaml
+read -p 'file: ' file
+cat /etc/netplan/`$file`
 sleep 5
 read -p 'interface: ' interface
-read -p ' `$interface` address: ' address
-read -p ' `$interface` netmask (/24): ' netmask
-/bin/cat<<EOM >> /etc/netplan/*-installer-config.yaml
+read -p `$interface`' address: ' address
+read -p `$interface`' netmask (24): ' netmask
+/bin/cat<<EOM >> /etc/netplan/`$file`
 # This is the network config written by 'subiquity'
 network:
   ethernets:
