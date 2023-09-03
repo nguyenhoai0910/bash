2e3b56de9e45228349a24d72a0a7de6b95f7fb80
Nguyen Hoai <50800569+nguyenhoai0910@users.noreply.github.com>
2023-09-03 10:23:38 +0700

    Update static-ipv4.sh

--- ubuntu22/static-ipv4.sh
+++ ubuntu22/static-ipv4.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
-ll /etc/netplan/*-installer-config.yaml
-read -p 'file: ' file
-cat /etc/netplan/`$file`
-sleep 5
-read -p 'interface: ' interface
-read -p `$interface`' address: ' address
-read -p `$interface`' netmask (24): ' netmask
-/bin/cat<<EOM >> /etc/netplan/`$file`
+hoainh@hoainh:~$ route
+Kernel IP routing table
+Destination     Gateway         Genmask         Flags Metric Ref    Use Iface
+default         192.168.153.2   0.0.0.0         UG    100    0        0 ens33
+10.0.0.0        0.0.0.0         255.255.255.0   U     0      0        0 ens34
+192.168.153.0   0.0.0.0         255.255.255.0   U     100    0        0 ens33
+192.168.153.2   0.0.0.0         255.255.255.255 UH    100    0        0 ens33
+
+hoainh@hoainh:~$ cat /etc/netplan/00-installer-config.yaml
 # This is the network config written by 'subiquity'
 network:
   ethernets:
     ens33:
       dhcp4: true
-    $interface:
+      routes:
+        - to: default
+          via: 192.168.153.2
+    ens34:
        addresses:
-                - $address/$netmask
+                - 10.0.0.11/24
   version: 2
-EOM
-sleep 5
-sudo netplan apply
